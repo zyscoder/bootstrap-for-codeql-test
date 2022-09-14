taltomer/bootstrap

refactor: Move `getElementFromSelector` & `getSelectorFromElement`
Move `getElementFromSelector` & getSelectorFromElement` inside selector-engine.js, in order to use SelectorEngine methods, avoiding raw querySelector usage

Feature: add `getMultipleElementsFromSelector` helper

js/src/tab.js
@@ -5,9 +5,9 @@
  * --------------------------------------------------------------------------
  */
 
-import { defineJQueryPlugin, getElementFromSelector, isDisabled, reflow } from './util/index'
+import { defineJQueryPlugin, isDisabled, reflow } from './util/index'
 import EventHandler from './dom/event-handler'
-import SelectorEngine from './dom/selector-engine'
+import { getElementFromSelector, SelectorEngine } from './dom/selector-engine'
 import BaseComponent from './base-component'
 
 /**

zyscoder/bootstrap-for-codeql-test

js/src/tab.js
@@ -5,9 +5,9 @@
  * --------------------------------------------------------------------------
  */
 
-import { defineJQueryPlugin, getElementFromSelector, getNextActiveElement, isDisabled } from './util/index'
+import { defineJQueryPlugin, getNextActiveElement, isDisabled } from './util/index'
 import EventHandler from './dom/event-handler'
-import SelectorEngine from './dom/selector-engine'
+import { getElementFromSelector, SelectorEngine } from './dom/selector-engine'
 import BaseComponent from './base-component'
 
 /**